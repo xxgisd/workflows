b0391f81e84d6c125caf4f6575fdebab155b1717
Xiuli Gong <xxgisd@rit.edu>
2015-01-29 11:06:18 -0500

    concatenate

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
 
  var coffeeSources = ['components/coffee/tagline.coffee'],
      destJsDir = 'components/scripts';
+
+ var jsSources = [
+     'components/scripts/rclick.js',
+     'components/scripts/pixgrid.js',
+     'components/scripts/tagline.js',
+     'components/scripts/template.js'
+ ];
+
+ var jsDevDest = 'builds/development/js';
 
 
 gulp.task('log', function(){
@@ -15,5 +25,13 @@ gulp.task('coffee', function(){
 	gulp.src(coffeeSources)
 	.pipe(coffee({bare: true})
 		.on('error', gutil.log))
-	.pipe(gulp.dest(destJsDir));
+	.pipe(gulp.dest(destJsDir))
+});
+
+gulp.task('js', function(){
+	gulp.src(jsSources)
+	    .pipe(concat('script.js'))
+	    .pipe(gulp.dest(jsDevDest))
+
+    
 });
