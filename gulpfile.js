80aa4a789459a0edbd7d91c5853ad145512ffb58
Xiuli Gong <xxgisd@rit.edu>
2015-01-29 10:52:17 -0500

    added coffee

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,19 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee');
+
+ var coffeeSources = ['components/coffee/tagline.coffee'],
+     destJsDir = 'components/scripts';
+
 
 gulp.task('log', function(){
 
      gutil.log('Workflows work very well');
+});
+
+gulp.task('coffee', function(){
+	gulp.src(coffeeSources)
+	.pipe(coffee({bare: true})
+		.on('error', gutil.log))
+	.pipe(gulp.dest(destJsDir));
 });
